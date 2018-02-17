0faf3201849f711e04cef00f5c692e18094145fd
Allan Vieira Ribeiro <allanvribeiro1@gmail.com>
2018-02-17 20:42:23 -0200

    Remoção do MDBootstrap e utilização somente do bootstrap

--- mediatheque/src/main/webapp/js/script.js
+++ mediatheque/src/main/webapp/js/script.js
@@ -15,3 +15,31 @@ function ativarNavegacaoCadastro(){
 	document.getElementById("login-nav").classList.remove("active");
 	document.getElementById("cadastro-nav").classList.add("active");
 }
+
+PrimeFaces.locales['pt'] = {  
+        closeText: 'Fechar',  
+        prevText: 'Anterior',  
+        nextText: 'Próximo',  
+        currentText: 'Início',  
+        monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],  
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun', 'Jul','Ago','Set','Out','Nov','Dez'],  
+        dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],  
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb'],  
+        dayNamesMin: ['D','S','T','Q','Q','S','S'],  
+        weekHeader: 'Semana',  
+        firstDay: 0,  
+        isRTL: false,  
+        showMonthAfterYear: false,  
+        yearSuffix: '',  
+        timeOnlyTitle: 'Só Horas',  
+        timeText: 'Tempo',  
+        hourText: 'Hora',  
+        minuteText: 'Minuto',  
+        secondText: 'Segundo',  
+        currentText: 'Data Atual',  
+        ampm: false,  
+        month: 'Mês',  
+        week: 'Semana',  
+        day: 'Dia',  
+        allDayText : 'Todo Dia'  
+    };
